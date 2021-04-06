9a5ac4f7eb082b4045bf7544a0e2b0676e789cfe
Fenghui.Xu <fenghui.xu@ly.com>
2021-04-06 15:38:08 +0800

    test

--- MongoDBConfigExtension.cs
+++ MongoDBConfigExtension.cs
@@ -89,53 +89,57 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             try
             {
-                var assembly = Assembly.GetEntryAssembly();
-                var mongoEntityTypes = new List<Type>();
-                var types = assembly.GetTypes();
-                foreach (var type in types)
-                {
-                    if (type != null && type.BaseType != null && type.BaseType.Equals(typeof(MongoEntity)))
-                    {
-                        mongoEntityTypes.Add(type);
-                    }
-                }
-                foreach (var type in mongoEntityTypes)
-                {
-                    var interface_type = typeof(IMongoDBCollection<>).MakeGenericType(type);
-                    var imple_type = typeof(MongoDBCollection<>).MakeGenericType(type);
-                    services.AddScoped(interface_type, imple_type);
-                }
+                var entry_assembly = Assembly.GetEntryAssembly();
+                var calling_assembly = Assembly.GetCallingAssembly();
+                var executing_assembly = Assembly.GetExecutingAssembly();
+
+                services.ServiceNow(entry_assembly, services.AddScoped);
+                services.ServiceNow(calling_assembly, services.AddScoped);
+                services.ServiceNow(executing_assembly, services.AddScoped);
             }
             catch (Exception ex)
             {
-                throw new Exception("exception when add scope instance for entiteis based on MongoDBEntity.", ex);
+                throw new Exception("exception when add Scope instance for entiteis based on MongoDBEntity.", ex);
             }
         }
+
         public static void AddMongoDBEntitySingleton(this IServiceCollection services)
         {
             try
             {
-                var assembly = Assembly.GetEntryAssembly();
-                var mongoEntityTypes = new List<Type>();
-                var types = assembly.GetTypes();
-                foreach (var type in types)
-                {
-                    if (type != null && type.BaseType != null && type.BaseType.Equals(typeof(MongoEntity)))
-                    {
-                        mongoEntityTypes.Add(type);
-                    }
-                }
-                foreach (var type in mongoEntityTypes)
-                {
-                    var interface_type = typeof(IMongoDBCollection<>).MakeGenericType(type);
-                    var imple_type = typeof(MongoDBCollection<>).MakeGenericType(type);
-                    services.AddSingleton(interface_type, imple_type);
-                }
+                var entry_assembly = Assembly.GetEntryAssembly();
+                var calling_assembly = Assembly.GetCallingAssembly();
+                var executing_assembly = Assembly.GetExecutingAssembly();
+
+                services.ServiceNow(entry_assembly, services.AddSingleton);
+                services.ServiceNow(calling_assembly, services.AddSingleton);
+                services.ServiceNow(executing_assembly, services.AddSingleton);
             }
             catch (Exception ex)
             {
-                throw new Exception("exception when add scope instance for entiteis based on MongoDBEntity.", ex);
+                throw new Exception("exception when add Singleton instance for entiteis based on MongoDBEntity.", ex);
             }
         }
+
+        private static void ServiceNow(this IServiceCollection services, Assembly assembly, Func<Type, Type, IServiceCollection> action)
+        {
+            var mongoEntityTypes = new List<Type>();
+            var types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                if (type != null && type.BaseType != null && type.BaseType.Equals(typeof(MongoEntity)))
+                {
+                    mongoEntityTypes.Add(type);
+                }
+            }
+            foreach (var type in mongoEntityTypes)
+            {
+                var interface_type = typeof(IMongoDBCollection<>).MakeGenericType(type);
+                var imple_type = typeof(MongoDBCollection<>).MakeGenericType(type);
+                action(interface_type, imple_type);
+            }
+        }
+
+
     }
 }
